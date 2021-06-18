f3b49157f3ad306d6abfa9ecd16ec2b9bfb479a5
AdivonSlav <efendic100@gmail.com>
2021-06-18 17:06:55 +0200

    First release

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-exports.LOGIN_USERNAME = '';
-exports.LOGIN_PASSWORD = '';
+exports.LOGIN_USERNAME = process.env.LOGIN_USERNAME;
+exports.LOGIN_PASSWORD = process.env.LOGIN_PASSWORD;
 
-exports.BOT_TOKEN = '';
-exports.WEBHOOK_ID = '';
-exports.WEBHOOK_TOKEN = '';
+exports.BOT_TOKEN = process.env.BOT_TOKEN;
+exports.WEBHOOK_ID = process.env.WEBHOOK_ID;
+exports.WEBHOOK_TOKEN = process.env.WEBHOOK_TOKEN;
+
+exports.BACKGROUND_IMAGE = process.env.BACKGROUND_IMAGE;
--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 const Discord = require('discord.js');
+const {LOGIN_USERNAME, LOGIN_PASSWORD, BOT_TOKEN, WEBHOOK_ID, WEBHOOK_TOKEN, BACKGROUND_IMAGE} = require('./config.js');
 
 const client = new Discord.Client();
-const {LOGIN_USERNAME, LOGIN_PASSWORD, BOT_TOKEN} = require('./config.js');
+const webhookClient = new Discord.WebhookClient(WEBHOOK_ID, WEBHOOK_TOKEN);
+
 const url = `https://www.fit.ba/student/login.aspx`;
+const news = {
+    title: "",
+    url: "",
+    date: "",
+    subject: "",
+    author: "",
+    description: ""
+}
 
 client.on('ready', () => {
+    client.user.setActivity('DLWMS', { type: 'WATCHING' })
     console.log(`Primed and ready!`);
 
     (async () => {
         while(true)  {
             await Scraper();
-            await sleep(5000);
+            await sleep(300000);
         }
     })();
 });
 
 client.on('message', msg => {
-    
+
 });
 
 async function pageLogin(page) {
@@ -51,29 +62,32 @@ async function Scraper() {
     
     const content = await page.content();
     const $ = cheerio.load(content);
-    const news = {
-        title: "",
-        date: "",
-        subject: "",
-        author: "",
-        description: ""
-    }  
+
+    const title = $("#lnkNaslov").slice(0,1).text();
+    const date = $("#lblDatum").slice(0,1).text();
+    if (title == news.title) {
+        if (date == news.date) {
+            return null;
+        }
+    }
 
     $('.newslist').slice(0, 1).each(function() {
         const title = $(this).find('#lnkNaslov').text();
+        const url = $(this).find('#lnkNaslov').attr('href');
         const date = $(this).find('#lblDatum').text();
         const subject = $(this).find('#lblPredmet').text();
         const author = $(this).find('#HyperLink9').text();
         const description = $(this).find('.abstract').text();
-        
+
         news.title = title;
+        news.url = url;
         news.date = date;
         news.subject = subject;
         news.author = author;
         news.description = description;
     })
 
-    console.log(news);
+    sendMessage();
 
     await page.goto("https://www.fit.ba/student/logout.aspx")
     browser.close();
@@ -82,6 +96,23 @@ async function Scraper() {
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function sendMessage() {
+    const newsEmbed = new Discord.MessageEmbed()
+        .setTitle(news.title)
+        .setURL(`https://www.fit.ba/student/${news.url}`)
+        .setAuthor(news.author)
+        .setDescription(news.description)
+        .setThumbnail(BACKGROUND_IMAGE)
+        .setTimestamp()
+        .setFooter('Mesh Scraper', '');
+
+    webhookClient.send('@everyone', {
+        embeds: [newsEmbed],
+    });
+
+    console.send('Sent new announcement!');
 }
 
 client.login(BOT_TOKEN);
